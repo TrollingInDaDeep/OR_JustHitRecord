76317f3c8298103979b77b84e11eaa7513cb3c33
TrollingInDaDeep <dorieder@gmx.ch>
2026-04-21 18:05:31 +0200

    stereo audio interleaving coded. but it doesn't work :) file is empty. now to troubleshoot

--- teensy/teensy_SD_recorder/teensy_SD_recorder.ino
+++ teensy/teensy_SD_recorder/teensy_SD_recorder.ino
@@ -21,29 +21,36 @@ float btnPressMillis; //ms Timestamp when btn was pressed
 int debounceMillis = 50; //button debounce time in ms
 
 //SDCard Stuff
-
 const char *session_root_dir = "/JHR"; //root directory where the files are stored
 const char *file_prefix = "/aufnahme_"; //prefix of rec file
 const char *file_suffix = ".wav"; //suffix of rec file
 char nextRecFileName[64]; //full name of next file to be recorded
 FsFile recFile; //Stream that will be saved to SD Card
-const int chipSelect = 10; //teensy audio board
+const int chipSelect = 10; //SD card on teensy audio board
 int fileCount = 0; //how many files are created in current recording Session
 
 const char* dirPrefix = "/Session_"; //prefix of the directory name
 int dirCount = 0; //counts how many directories there are, to create next directory
 char nextDirName[20]; //name of the next directory to be created
 
-//SD Write Buffering
-#define NBLOCKS 16
-#define SAMPLES_PER_BLOCK AUDIO_BLOCK_SAMPLES
+//Audio settings
+const int teensy_audio_memory = 120; //increase if unstable. how much memory is given to the audio:
+//50 – 200  → normal use
+//200 – 400 → heavy audio processing
+//400 – 600 → high-risk but sometimes OK
+
+#define NBLOCKS 16 //can be increased to 32 if unstable. will increase buffer
+#define SAMPLES_PER_BLOCK AUDIO_BLOCK_SAMPLES //use same sample per block number as in teensy audio library (128)
 
 int16_t buffer[NBLOCKS * SAMPLES_PER_BLOCK * 2]; // stereo
-int bufIndex = 0;
+uint32_t totalSamples = 0; //counts how many samples were recorded to measure total data file size
 
-uint32_t totalSamples = 0;
-
-//Audio I2s Teensy Audio Shield stuff
+//information for WAV file header
+uint32_t subChunk1Size = 16;
+uint16_t audioFormat   = 1;      // PCM
+uint16_t numChannels    = 2;      // stereo
+uint32_t sampleRate    = 44100;
+uint16_t bitsPerSample = 16;
 
 // GUItool: begin automatically generated code
 AudioInputI2S            i2s1;           //xy=882,406
@@ -56,31 +63,35 @@ AudioConnection          patchCord2(i2s1, 1, queueR, 0);
 //functions
 
 /// writes wav file header
-/// expects file and size
-// void writeWavHeader(File &f, uint32_t dataSize) {
-//   f.seek(0);
+/// expects file size
+/// AI generated and double checked with wikipedia and i can grasp it a bit but could never write this
+/// and yes there are libraries for that but don't get me started
+/// sorry :(
+void writeWavHeader(uint32_t dataSize) {
+  recFile.seek(0); //go to file start
 
-//   f.write("RIFF", 4);
-//   uint32_t chunkSize = 36 + dataSize;
-//   f.write((uint8_t*)&chunkSize, 4);
-//   f.write("WAVE", 4);
+  //write standardized expected data that is needed to make the file a WAV file
+  recFile.write("RIFF", 4);
+  uint32_t chunkSize = 36 + dataSize;
+  recFile.write((uint8_t*)&chunkSize, 4);
+  recFile.write("WAVE", 4);
 
-//   f.write("fmt ", 4);
+  recFile.write("fmt ", 4);
   
-//   uint32_t byteRate = sampleRate * numChannels * bitsPerSample / 8;
-//   uint16_t blockAlign = numChannels * bitsPerSample / 8;
+  uint32_t byteRate = sampleRate * numChannels * bitsPerSample / 8;
+  uint16_t blockAlign = numChannels * bitsPerSample / 8;
 
-//   f.write((uint8_t*)&subChunk1Size, 4);
-//   f.write((uint8_t*)&audioFormat, 2);
-//   f.write((uint8_t*)&numChannels, 2);
-//   f.write((uint8_t*)&sampleRate, 4);
-//   f.write((uint8_t*)&byteRate, 4);
-//   f.write((uint8_t*)&blockAlign, 2);
-//   f.write((uint8_t*)&bitsPerSample, 2);
+  recFile.write((uint8_t*)&subChunk1Size, 4);
+  recFile.write((uint8_t*)&audioFormat, 2);
+  recFile.write((uint8_t*)&numChannels, 2);
+  recFile.write((uint8_t*)&sampleRate, 4);
+  recFile.write((uint8_t*)&byteRate, 4);
+  recFile.write((uint8_t*)&blockAlign, 2);
+  recFile.write((uint8_t*)&bitsPerSample, 2);
 
-//   f.write("data", 4);
-//   f.write((uint8_t*)&dataSize, 4);
-// }
+  recFile.write("data", 4);
+  recFile.write((uint8_t*)&dataSize, 4);
+}
 
 /// starts SD card recording
 void record_start(){
@@ -89,10 +100,17 @@ void record_start(){
   } else {
     //we're fucked, but it'll be alright
   }
-  
-  //open output file in Write mode
+
+  //open output file in write mode
   recFile = SD.sdfs.open(nextRecFileName, O_WRITE | O_CREAT);
-  //audioStreamWAVToFile.begin(out, i2sStream);
+
+  // reserve WAV header. first 44 bytes will contain file information
+  for (int i = 0; i < 44; i++) recFile.write((uint8_t)0);
+
+  //start recording queues
+  queueL.begin();
+  queueR.begin();
+
   blnRecording = true;
   digitalWrite(ledPin, blnRecording);
   Serial.println("recording...");
@@ -100,12 +118,54 @@ void record_start(){
 
 /// stops SD card recording
 void record_end(){
+
+    queueL.end();
+    queueR.end();
+
+    //will not fetch the remaining blocks in the queue, so some milliseconds are lost at the end.
+
+    //calculate data file size, needed to write the WAV header
+    uint32_t dataSize = totalSamples * numChannels * (bitsPerSample / 8); //samples count, stereo(2 channels), 16bit(2 bytes)
+
+    //write wav header
+    writeWavHeader(dataSize);
+
     recFile.close();
     blnRecording = false;
     digitalWrite(ledPin, blnRecording);
     Serial.println("recording end");
 }
 
+///looped part of recording
+///streams audio to opened file while recording is active
+///right and left channel are interleaved (R L R L R L ...) 
+void recordingLoop() {
+  //crashes ---> //if (queueL.available() && queueR.available()) { // check both queues if they have data
+  if (queueL.available() >= NBLOCKS && queueR.available() >= NBLOCKS) { //check both queues if they have required amount of blocks inside
+    int idx = 0; //index which bit in the buffer we're at
+
+    for (int b = 0; b < NBLOCKS; b++) { //loop through blocks fast to free up queue
+      int16_t *l = (int16_t*)queueL.readBuffer(); //write block from left queue to l byte
+      int16_t *r = (int16_t*)queueR.readBuffer(); //write block from right queue to r byte
+
+      for (int i = 0; i < SAMPLES_PER_BLOCK; i++) { //loop through every sample in both l and r byte
+        //this will write the bits alternating into the buffer
+        buffer[idx++] = l[i]; //write l bit to buffer, jump one bit forward in buffer
+        buffer[idx++] = r[i]; //write r bit to buffer, jump one bit forward in buffer
+        totalSamples++; //increase total samples written. to count file size in the end. Will be multiplied by 2 as we have 2 channels
+      }
+      
+      //buffer is freed, so new audio data can come in even if SD write is not done yet
+      queueL.freeBuffer();
+      queueR.freeBuffer();
+    }
+
+    // actually write buffer to SD
+    // this might be slow, but the buffer can fill again as we've freed it before.
+    // audio data is still being retreived
+    recFile.write((uint8_t*)buffer, idx * 2);
+  }
+}
 
 /// flashes the LED with a pattern
 /// expects integer with the error number
@@ -220,6 +280,7 @@ void readBtn() {
     }
   }
 }
+
 void setup() {
   //LED Pin Modes
   pinMode(ledPin, OUTPUT);
@@ -237,7 +298,7 @@ void setup() {
     SD.sdfs.ls();
   }
 
-  AudioMemory(100); //give much memory
+  AudioMemory(teensy_audio_memory); //give much memory
   lastBtnState = digitalRead(recBtnPin);
 }
 
@@ -246,7 +307,7 @@ void loop() {
   
   //write audio stream to SD card if recording is armed
   if (blnRecording) {
-    //audioStreamWAVToFile.copy();
+    recordingLoop();
   }
 
   //read if a button has been pressed
